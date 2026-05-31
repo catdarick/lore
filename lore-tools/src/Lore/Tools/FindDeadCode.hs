c29fdbcb8b38901c95628ab6fd83ee9c7bcaf2c5
Alexey Kurdukov <kurdukov.alexey@gmail.com>
2026-06-01 01:31:38 +0200

    Extract dead code request resolution

--- lore-tools/src/Lore/Tools/FindDeadCode.hs
+++ lore-tools/src/Lore/Tools/FindDeadCode.hs
@@ -12,14 +12,13 @@ module Lore.Tools.FindDeadCode
   )
 where
 
-import qualified Data.Map.Strict as Map
 import Data.List (foldl')
 import Data.Maybe (mapMaybe)
+import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified GHC
-import GHC.Generics (Generic)
 import qualified Lore as Core
 import Lore
   ( DeadCodeOptions (..),
@@ -27,24 +26,19 @@ import Lore
     DeadDefinition (..),
     DefinitionSource (..),
     MonadLore,
-    Symbol (..),
-    resolveDefinitionSourceNamed,
   )
-import Lore.Tools.Config (LoreConfig (..), loadLoreConfig)
-import Lore.Tools.Internal.SymbolResolution
-  ( ResolvedSymbolQuery (..),
-    SymbolsResolved (resolvedQueries),
-    SymbolsUnresolved,
-    resolveUniqueSymbolQueries,
-    unresolvedSymbolQueriesMessage,
+import Lore.Tools.FindDeadCode.Request
+  ( FindDeadCodeFailureReason (..),
+    FindDeadCodeOptions (..),
+    ResolvedFindDeadCodeRequest (..),
+    resolveFindDeadCodeRequest,
   )
 import Lore.Tools.Render.Doc (LoreDoc, ToLoreDoc (toLoreDoc), paragraph)
-import Lore.Tools.Render.Text (quoteText, renderModuleName, renderSymbolName)
+import Lore.Tools.Render.Text (renderModuleName, renderSymbolName)
 import Lore.Tools.Result
   ( Paginated (..),
     PaginationRenderConfig (..),
     PartialLoadWarning,
-    PageRequest (..),
     ToolRun (..),
     defaultPageRequest,
     loadedSessionPartialWarning,
@@ -52,12 +46,6 @@ import Lore.Tools.Result
     paginationSummaryDoc,
     withLoadedSession,
   )
-
-data FindDeadCodeOptions = FindDeadCodeOptions
-  { findDeadCodeModules :: Maybe [Text],
-    findDeadCodePageRequest :: Maybe PageRequest
-  }
-  deriving stock (Eq, Show, Generic)
 
 type FindDeadCodeResult = ToolRun FindDeadCodeOutput
 
@@ -69,13 +57,6 @@ data FindDeadCodeFailure = FindDeadCodeFailure
   { findDeadCodeFailureReason :: FindDeadCodeFailureReason,
     findDeadCodeFailurePartialLoadWarning :: Maybe PartialLoadWarning
   }
-
-data FindDeadCodeFailureReason
-  = FindDeadCodeUnresolvedModules [Text]
-  | FindDeadCodeInvalidConfig Text
-  | FindDeadCodeUnresolvedAliveModules [Text]
-  | FindDeadCodeUnresolvedSymbols SymbolsUnresolved
-  | FindDeadCodeInvalidAliveSymbols [Text]
 
 data FindDeadCodeReady = FindDeadCodeReady
   { findDeadCodeSummary :: Text,
@@ -91,198 +72,44 @@ data RenderedDeadDefinition = RenderedDeadDefinition
   }
 
 findDeadCode :: (MonadLore m) => FindDeadCodeOptions -> m FindDeadCodeResult
-findDeadCode FindDeadCodeOptions {findDeadCodeModules, findDeadCodePageRequest} = do
+findDeadCode options@FindDeadCodeOptions {findDeadCodePageRequest} = do
   withLoadedSession \session -> do
     let partialLoadWarning =
           loadedSessionPartialWarning session "Dead-code results may be incomplete."
-    eiTargetModules <- resolveOptionalLoadedHomeModules findDeadCodeModules
-    case eiTargetModules of
-      Left unresolvedModules ->
+    eiRequest <- resolveFindDeadCodeRequest options
+    case eiRequest of
+      Left failureReason ->
         pure $
           FindDeadCodeFailed
             FindDeadCodeFailure
-              { findDeadCodeFailureReason = FindDeadCodeUnresolvedModules unresolvedModules,
+              { findDeadCodeFailureReason = failureReason,
                 findDeadCodeFailurePartialLoadWarning = partialLoadWarning
               }
-      Right targetModules -> do
-        eiConfig <- loadLoreConfig
-        case eiConfig of
-          Left configError ->
-            pure $
-              FindDeadCodeFailed
-                FindDeadCodeFailure
-                  { findDeadCodeFailureReason = FindDeadCodeInvalidConfig configError,
-                    findDeadCodeFailurePartialLoadWarning = partialLoadWarning
-                  }
-          Right config -> do
-            eiAliveModules <- resolveLoadedHomeModulesByName config.loreConfigAliveModules
-            case eiAliveModules of
-              Left unresolvedAliveModules ->
-                pure $
-                  FindDeadCodeFailed
-                    FindDeadCodeFailure
-                      { findDeadCodeFailureReason = FindDeadCodeUnresolvedAliveModules unresolvedAliveModules,
-                        findDeadCodeFailurePartialLoadWarning = partialLoadWarning
-                      }
-              Right resolvedAliveModules -> do
-                eiAliveNames <- resolveAliveRootNames config.loreConfigAliveSymbols
-                case eiAliveNames of
-                  Left unresolvedSymbols ->
-                    pure $
-                      FindDeadCodeFailed
-                        FindDeadCodeFailure
-                          { findDeadCodeFailureReason = FindDeadCodeUnresolvedSymbols unresolvedSymbols,
-                            findDeadCodeFailurePartialLoadWarning = partialLoadWarning
-                          }
-                  Right (Left invalidAliveSymbols) ->
-                    pure $
-                      FindDeadCodeFailed
-                        FindDeadCodeFailure
-                          { findDeadCodeFailureReason = FindDeadCodeInvalidAliveSymbols invalidAliveSymbols,
-                            findDeadCodeFailurePartialLoadWarning = partialLoadWarning
-                          }
-                  Right (Right aliveRootNames) -> do
-                    deadCodeResult <-
-                      Core.findDeadCode
-                        Core.DeadCodeOptions
-                          { deadCodeTargetModules = targetModules,
-                            deadCodeAliveModules = resolvedAliveModules,
-                            deadCodeAliveNames = aliveRootNames
-                          }
-                    let deadDefinitions =
-                          filterRenderableDeadDefinitions deadCodeResult.deadCodeDeadDefinitions
-                        hasDeadDefinitions = not (null deadDefinitions)
-                        summaryLine = renderSummary deadCodeResult deadDefinitions
-                        maybePage = paginateItemsWithPageRequest pageRequest deadDefinitions
-                    pure $
-                      FindDeadCodeReadyResult
-                        FindDeadCodeReady
-                          { findDeadCodeSummary = summaryLine,
-                            findDeadCodeHasDeadDefinitions = hasDeadDefinitions,
-                            findDeadCodePage = renderDeadDefinitionPage <$> maybePage,
-                            findDeadCodeWarnings = deadCodeResult.deadCodeWarnings,
-                            findDeadCodePartialLoadWarning = partialLoadWarning
-                          }
+      Right resolvedRequest -> do
+        deadCodeResult <-
+          Core.findDeadCode
+            Core.DeadCodeOptions
+              { deadCodeTargetModules = resolvedRequest.resolvedDeadCodeTargetModules,
+                deadCodeAliveModules = resolvedRequest.resolvedDeadCodeAliveModules,
+                deadCodeAliveNames = resolvedRequest.resolvedDeadCodeAliveNames
+              }
+        let deadDefinitions =
+              filterRenderableDeadDefinitions deadCodeResult.deadCodeDeadDefinitions
+            hasDeadDefinitions = not (null deadDefinitions)
+            summaryLine = renderSummary deadCodeResult deadDefinitions
+            maybePage = paginateItemsWithPageRequest pageRequest deadDefinitions
+        pure $
+          FindDeadCodeReadyResult
+            FindDeadCodeReady
+              { findDeadCodeSummary = summaryLine,
+                findDeadCodeHasDeadDefinitions = hasDeadDefinitions,
+                findDeadCodePage = renderDeadDefinitionPage <$> maybePage,
+                findDeadCodeWarnings = deadCodeResult.deadCodeWarnings,
+                findDeadCodePartialLoadWarning = partialLoadWarning
+              }
   where
     pageRequest =
       maybe defaultPageRequest id findDeadCodePageRequest
-
-resolveOptionalLoadedHomeModules ::
-  (MonadLore m) =>
-  Maybe [Text] ->
-  m (Either [Text] (Maybe (Set.Set GHC.Module)))
-resolveOptionalLoadedHomeModules maybeModuleNames =
-  case maybeModuleNames of
-    Nothing ->
-      pure (Right Nothing)
-    Just moduleNames -> do
-      eiResolved <- resolveLoadedHomeModulesByName moduleNames
-      pure (fmap Just eiResolved)
-
-resolveLoadedHomeModulesByName ::
-  (MonadLore m) =>
-  [Text] ->
-  m (Either [Text] (Set.Set GHC.Module))
-resolveLoadedHomeModulesByName requestedModuleNames = do
-  moduleGraph <- GHC.getModuleGraph
-  let loadedModules =
-        map GHC.ms_mod (GHC.mgModSummaries moduleGraph)
-      loadedModulesByName =
-        Map.fromListWith
-          (++)
-          [ (renderModuleName module_, [module_])
-          | module_ <- loadedModules
-          ]
-  pure $
-    let resolutions =
-          map (resolveOne loadedModulesByName) requestedModuleNames
-        unresolvedMessages =
-          [ message
-          | Left message <- resolutions
-          ]
-        resolvedModules =
-          [ module_
-          | Right module_ <- resolutions
-          ]
-     in if null unresolvedMessages
-          then Right (Set.fromList resolvedModules)
-          else Left unresolvedMessages
-  where
-    resolveOne loadedModulesByName requestedModuleName =
-      case Map.lookup requestedModuleName loadedModulesByName of
-        Nothing ->
-          Left ("Module " <> quoteText requestedModuleName <> " is not present in the loaded home module graph.")
-        Just [module_] ->
-          Right module_
-        Just _ ->
-          Left ("Module " <> quoteText requestedModuleName <> " is ambiguous in the loaded home module graph.")
-
-resolveAliveRootNames ::
-  (MonadLore m) =>
-  [Text] ->
-  m (Either SymbolsUnresolved (Either [Text] (Set.Set GHC.Name)))
-resolveAliveRootNames queries =
-  if null queries
-    then pure (Right (Right Set.empty))
-    else do
-      eiResolved <- resolveUniqueSymbolQueries queries
-      case eiResolved of
-        Left unresolved ->
-          pure (Left unresolved)
-        Right resolved -> do
-          validations <- mapM resolveOneAliveQuery resolved.resolvedQueries
-          let invalidMessages =
-                [ invalidMessage
-                | Left invalidMessage <- validations
-                ]
-              validRootNames =
-                [ name
-                | Right name <- validations
-                ]
-          pure $
-            if null invalidMessages
-              then Right (Right (Set.fromList validRootNames))
-              else Right (Left invalidMessages)
-
-resolveOneAliveQuery ::
-  (MonadLore m) =>
-  ResolvedSymbolQuery ->
-  m (Either Text GHC.Name)
-resolveOneAliveQuery resolvedQuery = do
-  maybeAliveName <- firstAliveName candidates
-  pure $
-    case maybeAliveName of
-      Just aliveName ->
-        Right aliveName
-      Nothing ->
-        Left $
-          "Symbol "
-            <> quoteText resolvedQuery.queryText
-            <> " resolved, but it is not a loaded home definition and cannot be used as a dead-code root."
-  where
-    candidates =
-      dedupeNames [resolvedQuery.resolvedSymbol.name, resolvedQuery.resolvedRootName]
-
-firstAliveName :: (MonadLore m) => [GHC.Name] -> m (Maybe GHC.Name)
-firstAliveName [] =
-  pure Nothing
-firstAliveName (name : restNames) = do
-  maybeSource <- resolveDefinitionSourceNamed name
-  case maybeSource of
-    Just _ ->
-      pure (Just name)
-    Nothing ->
-      firstAliveName restNames
-
-dedupeNames :: [GHC.Name] -> [GHC.Name]
-dedupeNames =
-  foldr
-    ( \name deduped ->
-        if name `elem` deduped
-          then deduped
-          else name : deduped
-    )
-    []
 
 renderSummary :: DeadCodeResult -> [DeadDefinition] -> Text
 renderSummary deadCodeResult deadDefinitions =
@@ -320,19 +147,6 @@ instance ToLoreDoc FindDeadCodeFailure where
       [ toLoreDoc failed.findDeadCodeFailureReason,
         maybe mempty toLoreDoc failed.findDeadCodeFailurePartialLoadWarning
       ]
-
-instance ToLoreDoc FindDeadCodeFailureReason where
-  toLoreDoc = \case
-    FindDeadCodeUnresolvedModules unresolved ->
-      paragraph (T.intercalate "\n" unresolved)
-    FindDeadCodeInvalidConfig configError ->
-      paragraph configError
-    FindDeadCodeUnresolvedAliveModules unresolved ->
-      paragraph (T.intercalate "\n" unresolved)
-    FindDeadCodeUnresolvedSymbols unresolvedSymbols ->
-      paragraph (unresolvedSymbolQueriesMessage unresolvedSymbols)
-    FindDeadCodeInvalidAliveSymbols invalidSymbols ->
-      paragraph (T.intercalate "\n" invalidSymbols)
 
 instance ToLoreDoc FindDeadCodeReady where
   toLoreDoc = renderFindDeadCodeReady
---
+++ lore-tools/src/Lore/Tools/FindDeadCode/Request.hs
@@ -0,0 +1,214 @@
+module Lore.Tools.FindDeadCode.Request
+  ( FindDeadCodeFailureReason (..),
+    FindDeadCodeOptions (..),
+    ResolvedFindDeadCodeRequest (..),
+    resolveFindDeadCodeRequest,
+  )
+where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified GHC
+import GHC.Generics (Generic)
+import Lore
+  ( MonadLore,
+    Symbol (..),
+    resolveDefinitionSourceNamed,
+  )
+import Lore.Tools.Config (LoreConfig (..), loadLoreConfig)
+import Lore.Tools.Internal.SymbolResolution
+  ( ResolvedSymbolQuery (..),
+    SymbolsResolved (resolvedQueries),
+    SymbolsUnresolved,
+    resolveUniqueSymbolQueries,
+    unresolvedSymbolQueriesMessage,
+  )
+import Lore.Tools.Render.Doc (ToLoreDoc (toLoreDoc), paragraph)
+import Lore.Tools.Render.Text (quoteText, renderModuleName)
+import Lore.Tools.Result (PageRequest)
+
+data FindDeadCodeOptions = FindDeadCodeOptions
+  { findDeadCodeModules :: Maybe [Text],
+    findDeadCodePageRequest :: Maybe PageRequest
+  }
+  deriving stock (Eq, Show, Generic)
+
+data FindDeadCodeFailureReason
+  = FindDeadCodeUnresolvedModules [Text]
+  | FindDeadCodeInvalidConfig Text
+  | FindDeadCodeUnresolvedAliveModules [Text]
+  | FindDeadCodeUnresolvedSymbols SymbolsUnresolved
+  | FindDeadCodeInvalidAliveSymbols [Text]
+
+instance ToLoreDoc FindDeadCodeFailureReason where
+  toLoreDoc = \case
+    FindDeadCodeUnresolvedModules unresolved ->
+      paragraph (T.intercalate "\n" unresolved)
+    FindDeadCodeInvalidConfig configError ->
+      paragraph configError
+    FindDeadCodeUnresolvedAliveModules unresolved ->
+      paragraph (T.intercalate "\n" unresolved)
+    FindDeadCodeUnresolvedSymbols unresolvedSymbols ->
+      paragraph (unresolvedSymbolQueriesMessage unresolvedSymbols)
+    FindDeadCodeInvalidAliveSymbols invalidSymbols ->
+      paragraph (T.intercalate "\n" invalidSymbols)
+
+data ResolvedFindDeadCodeRequest = ResolvedFindDeadCodeRequest
+  { resolvedDeadCodeTargetModules :: Maybe (Set.Set GHC.Module),
+    resolvedDeadCodeAliveModules :: Set.Set GHC.Module,
+    resolvedDeadCodeAliveNames :: Set.Set GHC.Name
+  }
+
+resolveFindDeadCodeRequest ::
+  (MonadLore m) =>
+  FindDeadCodeOptions ->
+  m (Either FindDeadCodeFailureReason ResolvedFindDeadCodeRequest)
+resolveFindDeadCodeRequest FindDeadCodeOptions {findDeadCodeModules} = do
+  eiTargetModules <- resolveOptionalLoadedHomeModules findDeadCodeModules
+  case eiTargetModules of
+    Left unresolvedModules ->
+      pure (Left (FindDeadCodeUnresolvedModules unresolvedModules))
+    Right targetModules -> do
+      eiConfig <- loadLoreConfig
+      case eiConfig of
+        Left configError ->
+          pure (Left (FindDeadCodeInvalidConfig configError))
+        Right config -> do
+          eiAliveModules <- resolveLoadedHomeModulesByName config.loreConfigAliveModules
+          case eiAliveModules of
+            Left unresolvedAliveModules ->
+              pure (Left (FindDeadCodeUnresolvedAliveModules unresolvedAliveModules))
+            Right resolvedAliveModules -> do
+              eiAliveNames <- resolveAliveRootNames config.loreConfigAliveSymbols
+              pure $
+                case eiAliveNames of
+                  Left unresolvedSymbols ->
+                    Left (FindDeadCodeUnresolvedSymbols unresolvedSymbols)
+                  Right (Left invalidAliveSymbols) ->
+                    Left (FindDeadCodeInvalidAliveSymbols invalidAliveSymbols)
+                  Right (Right aliveRootNames) ->
+                    Right
+                      ResolvedFindDeadCodeRequest
+                        { resolvedDeadCodeTargetModules = targetModules,
+                          resolvedDeadCodeAliveModules = resolvedAliveModules,
+                          resolvedDeadCodeAliveNames = aliveRootNames
+                        }
+
+resolveOptionalLoadedHomeModules ::
+  (MonadLore m) =>
+  Maybe [Text] ->
+  m (Either [Text] (Maybe (Set.Set GHC.Module)))
+resolveOptionalLoadedHomeModules maybeModuleNames =
+  case maybeModuleNames of
+    Nothing ->
+      pure (Right Nothing)
+    Just moduleNames -> do
+      eiResolved <- resolveLoadedHomeModulesByName moduleNames
+      pure (fmap Just eiResolved)
+
+resolveLoadedHomeModulesByName ::
+  (MonadLore m) =>
+  [Text] ->
+  m (Either [Text] (Set.Set GHC.Module))
+resolveLoadedHomeModulesByName requestedModuleNames = do
+  moduleGraph <- GHC.getModuleGraph
+  let loadedModules =
+        map GHC.ms_mod (GHC.mgModSummaries moduleGraph)
+      loadedModulesByName =
+        Map.fromListWith
+          (++)
+          [ (renderModuleName module_, [module_])
+          | module_ <- loadedModules
+          ]
+  pure $
+    let resolutions =
+          map (resolveOne loadedModulesByName) requestedModuleNames
+        unresolvedMessages =
+          [ message
+          | Left message <- resolutions
+          ]
+        resolvedModules =
+          [ module_
+          | Right module_ <- resolutions
+          ]
+     in if null unresolvedMessages
+          then Right (Set.fromList resolvedModules)
+          else Left unresolvedMessages
+  where
+    resolveOne loadedModulesByName requestedModuleName =
+      case Map.lookup requestedModuleName loadedModulesByName of
+        Nothing ->
+          Left ("Module " <> quoteText requestedModuleName <> " is not present in the loaded home module graph.")
+        Just [module_] ->
+          Right module_
+        Just _ ->
+          Left ("Module " <> quoteText requestedModuleName <> " is ambiguous in the loaded home module graph.")
+
+resolveAliveRootNames ::
+  (MonadLore m) =>
+  [Text] ->
+  m (Either SymbolsUnresolved (Either [Text] (Set.Set GHC.Name)))
+resolveAliveRootNames queries =
+  if null queries
+    then pure (Right (Right Set.empty))
+    else do
+      eiResolved <- resolveUniqueSymbolQueries queries
+      case eiResolved of
+        Left unresolved ->
+          pure (Left unresolved)
+        Right resolved -> do
+          validations <- mapM resolveOneAliveQuery resolved.resolvedQueries
+          let invalidMessages =
+                [ invalidMessage
+                | Left invalidMessage <- validations
+                ]
+              validRootNames =
+                [ name
+                | Right name <- validations
+                ]
+          pure $
+            if null invalidMessages
+              then Right (Right (Set.fromList validRootNames))
+              else Right (Left invalidMessages)
+
+resolveOneAliveQuery ::
+  (MonadLore m) =>
+  ResolvedSymbolQuery ->
+  m (Either Text GHC.Name)
+resolveOneAliveQuery resolvedQuery = do
+  maybeAliveName <- firstAliveName candidates
+  pure $
+    case maybeAliveName of
+      Just aliveName ->
+        Right aliveName
+      Nothing ->
+        Left $
+          "Symbol "
+            <> quoteText resolvedQuery.queryText
+            <> " resolved, but it is not a loaded home definition and cannot be used as a dead-code root."
+  where
+    candidates =
+      dedupeNames [resolvedQuery.resolvedSymbol.name, resolvedQuery.resolvedRootName]
+
+firstAliveName :: (MonadLore m) => [GHC.Name] -> m (Maybe GHC.Name)
+firstAliveName [] =
+  pure Nothing
+firstAliveName (name : restNames) = do
+  maybeSource <- resolveDefinitionSourceNamed name
+  case maybeSource of
+    Just _ ->
+      pure (Just name)
+    Nothing ->
+      firstAliveName restNames
+
+dedupeNames :: [GHC.Name] -> [GHC.Name]
+dedupeNames =
+  foldr
+    ( \name deduped ->
+        if name `elem` deduped
+          then deduped
+          else name : deduped
+    )
+    []
